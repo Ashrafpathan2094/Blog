89dce37ed07c69c0b5cc30ac3b4a2db56eb19e3e
Ashrafpathan2094 <minazpathan2094@gmail.com>
2022-01-30 20:54:30 +0530

    Changes to be committed:

--- blog_post/models.py
+++ blog_post/models.py
@@ -10,3 +10,4 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=1000000000)
     created_at = models.DateTimeField(default=datetime.now,blank=True)
+    
--- blog_post/urls.py
+++ blog_post/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('logout',views.logout,name='logout'),
     path('register',views.register,name='register'),
     path('post/<str:pk>',views.post,name='post'),
-    path('user_profile/',views.user_profile,name='user_profile'),
+    path('user_profile/<str:username>/',views.user_profile,name='user_profile'),
     
 ]
